9a10151c10092b92c64ce4b5d9615cb9c2e4f386
Ivan Jesús Torres Rodriguez <itorres@iri.upc.edu>
2022-11-18 13:44:34 +0100

    Changes

--- tree_transform/scripts/transform.py
+++ tree_transform/scripts/transform.py
@@ -1,34 +1,41 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 # pub-sub node to transform detected object into tree reference frame
 
 import rospkg
 import rospy
 import tf as transf
+from data_fusion_node.msg import PoseArrayId
+from apriltag_ros.msg import AprilTagDetectionArray
 from geometry_msgs.msg import PoseArray
-from tree_transform.srv import tree_transformResponse
+from tree_transform.srv import tree_transformService, tree_transformResponse
 
 
-grapes_wrt_tag = PoseArray()
+
 
 def transform_req(req):
+    rospy.logwarn("AQUI 0")
+    grapes_wrt_tag_t = PoseArray()
     # AprilTagDetectionArray :custom message with header + detections,
     # detections is a custom message with id , size, pose
+
     apriltag = req.tag
     grapes_wrt_camera = req.detection # geometry_msgs/PoseArray
+    rospy.logwarn("AQUI 1")
     listener = transf.TransformListener()
     listener.waitForTransform(apriltag.header.frame_id,grapes_wrt_camera.header.frame_id,grapes_wrt_camera.header.stamp,rospy.Duration(3.0))
-    grapes_wrt_tag.header.stamp = listener.getLatestCommonTime(apriltag.header.frame_id,grapes_wrt_camera.header.frame_id)
-    grapes_wrt_tag.header.frame_id = apriltag.header.frame_id
+    grapes_wrt_tag_t.header.stamp = listener.getLatestCommonTime(apriltag.header.frame_id,grapes_wrt_camera.header.frame_id)
+    grapes_wrt_tag_t.header.frame_id = apriltag.header.frame_id
 
     for i in range(grapes_wrt_camera.poses): # grapes_wrt_camera.header.frame_id[i]
-        grapes_wrt_tag.poses.append(listener.transformPose(apriltag.header.frame_id,grapes_wrt_camera.poses[i]))
+        grapes_wrt_tag_t.poses.append(listener.transformPose(apriltag.header.frame_id,grapes_wrt_camera.poses[i]))
 
-
-    return tree_transformResponse(grapes_wrt_tag=grapes_wrt_tag)
+    rospy.logwarn("AQUI 2")
+    return tree_transformResponse(grapes_wrt_tag=grapes_wrt_tag_t)
 
 def tree_transf_server():
-    rospy.init_node('tree_transform_node')
-    s = rospy.Service('tree_transf_srv', tree_transformResponse, transform_req)
+    rospy.init_node('tree_transform_node', anonymous=True)
+    rospy.logwarn("AQUI Marco")
+    s = rospy.Service('tree_transf_srv', tree_transformService, transform_req)
     rospy.spin()
 
 if __name__ == '__main__':
--- tree_transform/scripts/tree_transf_client.py
+++ tree_transform/scripts/tree_transf_client.py
@@ -3,17 +3,25 @@
 import rospy
 import rospkg
 
-from tree_transform.srv import tree_transform as TransformResponse
-from geometry_msgs.msg import PoseArray
+from tree_transform.srv import tree_transformService, tree_transformResponse
+from data_fusion_node.msg import PoseArrayId
+from apriltag_ros.msg import AprilTagDetectionArray
 
 def get_tree_transf_client():
     rospy.wait_for_service('tree_transf_srv')
     try:
-        client_handle = rospy.ServiceProxy('tree_transf_srv', TransformResponse)
-        response1 = client_handle()
+        rospy.logwarn("here")
+        client_handle = rospy.ServiceProxy('tree_transf_srv', tree_transformService)
+	# wait topic
+        detections=rospy.wait_for_message("/poses_pfc", PoseArrayId)
+        tag=rospy.wait_for_message("/tag_detections", AprilTagDetectionArray)
+
+        response1 = client_handle(tag,detections.poses)
+        rospy.logwarn(response1)
         return response1
-    except rospy.ServiceException, e:
-        print "Service call failed: %s"%e
+    except rospy.ServiceException as e:
+        rospy.logerr("Service call failed: %s"%e)
 
 if __name__ == "__main__":
+    rospy.init_node('tree_transform_client')
     get_tree_transf_client()
