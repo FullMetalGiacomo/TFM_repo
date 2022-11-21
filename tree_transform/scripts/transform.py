d22cab9eb64cfa770c6257f19bb2944935682a95
FullMetalGiacomo <marco.giacchetti@estudiantat.upc.edu>
2022-11-21 14:22:44 +0100

    Transformation Service first version

--- tree_transform/scripts/transform.py
+++ tree_transform/scripts/transform.py
@@ -16,6 +16,8 @@ from geometry_msgs.msg import PoseArray # importing custom message PoseArray
 from geometry_msgs.msg import PoseStamped  # importing custom message PoseStamped
 import rospkg
 import rospy
+import numpy as np
+
 
 
 
@@ -34,28 +36,34 @@ def transform_req(req): # here we call the handler
     listener = transf.TransformListener() # creating the transform listener
     # waiting for the transform between "tag_0" ( "tag_i") and the camera frame apriltag.detections[i].id[0]
     ################################################################################################ my risky attempt
-    listener.waitForTransform("tag_"+str(apriltag.detections[0].id[0]),grapes_wrt_camera.header.frame_id,grapes_wrt_camera.header.stamp,rospy.Duration(3.0))
+    
     dist=np.zeros((len(grapes_wrt_camera.poses),len(apriltag.detections))) # array containing rows of # grapes and cols of # tags
     for i in range(len(apriltag.detections)):
-        x_tag = apriltag.detection[i].pose.pose.position.x
-        y_tag = apriltag.detection[i].pose.pose.position.y
-        z_tag = apriltag.detection[i].pose.pose.position.z
+        x_tag = apriltag.detections[i].pose.pose.pose.position.x
+        y_tag = apriltag.detections[i].pose.pose.pose.position.y
+        z_tag = apriltag.detections[i].pose.pose.pose.position.z
         for j in range(len(grapes_wrt_camera.poses)):
             x_grape = grapes_wrt_camera.poses[j].position.x
             y_grape = grapes_wrt_camera.poses[j].position.y
             z_grape = grapes_wrt_camera.poses[j].position.z
-            dist[j,i]=np.sqrt((x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2) # building distance matrix
+            dist[j,i]=np.sqrt((x_grape - x_tag)**2 + (y_grape - y_tag)**2 + (z_grape - z_tag)**2) # building distance matrix
 
     idx_tag=np.argmin(dist, axis=1) # finds min index by rows "contains the tags id"
     #idx is a vector composed like this [0 1 0 1 2 2 1 0] where the value corrisponds to the tag and the idx position to the grape.
     #d_min=dist.min(axis=1)# find minumum distances " contains the minumum dist"
-    for k in range(len(idx)):
-        for t in range(len(grapes_wrt_camera.poses))
+    for k in range(len(apriltag.detections)):
+
+        for t in range(len(grapes_wrt_camera.poses)):
+   	    rospy.logwarn(t)
             grapes_wrt_tag_s = PoseStamped()
+            listener.waitForTransform(grapes_wrt_camera.header.frame_id,"tag_"+str(apriltag.detections[k].id[0]),rospy.Time.now(),rospy.Duration(1.0))
             grapes_wrt_tag_s.header.stamp = listener.getLatestCommonTime("tag_"+str(apriltag.detections[k].id[0]),grapes_wrt_camera.header.frame_id) # taking the time
-            grapes_wrt_tag_s.header.frame_id = "tag_"+str(apriltag.detections[k].id[0])# creating the frame ID
+            grapes_wrt_tag_s.header.frame_id = grapes_wrt_camera.header.frame_id # creating the frame ID
             grapes_wrt_tag_s.pose=grapes_wrt_camera.poses[t] # taking each pose
-            grapes_wrt_tag_t.poses.append(listener.transformPose("tag_"+str(apriltag.detections[k].id[0]),grapes_wrt_tag_s).pose) # transformming and appending the new pose
+            grapes_wrt_tag_s=listener.transformPose("tag_"+str(apriltag.detections[k].id[0]),grapes_wrt_tag_s) # taking each pose
+            # grapes_wrt_tag_t.poses.append(listener.transformPose("tag_"+str(apriltag.detections[k].id[0]),grapes_wrt_tag_s).pose) # transformming and appending the new pose
+            grapes_wrt_tag_t.poses.append(grapes_wrt_tag_s) # transformming and appending the new pose
+ # transformming and appending the new pose
 
     rospy.logwarn(grapes_wrt_tag_t)
 
@@ -77,7 +85,7 @@ def transform_req(req): # here we call the handler
 def tree_transf_server():
     rospy.init_node('tree_transform_node', anonymous=True) # initialize the service node
     rospy.logwarn("AQUI Marco")
-    s = rospy.Service('tree_transf_srv',  tree_transformService, transform_req) # rospy.Service(name, service_class, handler)
+    s = rospy.Service('tree_transf_srv',  tree_transformService, transform_req) 
     rospy.spin()
 
 if __name__ == '__main__':
--- tree_transform/scripts/tree_transf_client.py
+++ tree_transform/scripts/tree_transf_client.py
@@ -6,6 +6,8 @@ import rospkg
 from tree_transform.srv import tree_transformService, tree_transformServiceResponse # importing the srv file top and bottom
 from data_fusion_node.msg import PoseArrayId # importing custom message of Grapes pose arrays
 from apriltag_ros.msg import AprilTagDetectionArray # importing the custom message of apriltags
+import message_filters
+
 
 def get_tree_transf_client():
     rospy.wait_for_service('tree_transf_srv') # wait for service to be available
@@ -13,14 +15,14 @@ def get_tree_transf_client():
         rospy.logwarn("here") # service is available
         client_handle = rospy.ServiceProxy('tree_transf_srv', tree_transformService) # calling the service that we want to call
      	# wait topic
-        tag=rospy.wait_for_message("/tag_detections", AprilTagDetectionArray) # we wait for AprilTagDetectionArray message
         detections=rospy.wait_for_message("/poses_pfc", PoseArrayId) # we wait for the PoseArrayID message
+        tag=rospy.wait_for_message("/tag_detections", AprilTagDetectionArray) # we wait for AprilTagDetectionArray message
 
         #rospy.logwarn(detections.poses)
         rospy.logwarn("here 1")
         response1 = client_handle(tag,detections.poses) # passing the two received messages to the service and saving the response
         rospy.logwarn("here 2")
-        rospy.logwarn(response1) 
+        rospy.logwarn(response1)
         return response1
     except rospy.ServiceException as e:
         rospy.logerr("Service call failed: %s"%e)
@@ -28,3 +30,4 @@ def get_tree_transf_client():
 if __name__ == "__main__":
     rospy.init_node('tree_transform_client') # initializing client
     get_tree_transf_client()
+
