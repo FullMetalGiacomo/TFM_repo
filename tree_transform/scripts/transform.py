293b45c2eb3a96ce0a22cb001e9e8b1da32c633a
FullMetalGiacomo <marco.giacchetti@estudiantat.upc.edu>
2022-11-21 11:53:53 +0100

    Implemented multiple trees

--- tree_transform/scripts/transform.py
+++ tree_transform/scripts/transform.py
@@ -4,50 +4,80 @@
 
 import tf as transf
 
-import sys
-print(sys.version)
+# import sys
+# print(sys.version)
 # tree_transform
-from tree_transform.srv import tree_transformService, tree_transformServiceResponse
+from tree_transform.srv import tree_transformService, tree_transformServiceResponse # importing srv up and bottom
 
-from data_fusion_node.msg import PoseArrayId
-from apriltag_ros.msg import AprilTagDetectionArray
-from geometry_msgs.msg import PoseArray
-from geometry_msgs.msg import PoseStamped
+from data_fusion_node.msg import PoseArrayId # importing custom message PoseArrayID
+from apriltag_ros.msg import AprilTagDetectionArray # importing custom message AprilTagDetectionArray
+from tree_transform.msg import PoseStampedArray # importing custom message PoseStampedArray
+from geometry_msgs.msg import PoseArray # importing custom message PoseArray
+from geometry_msgs.msg import PoseStamped  # importing custom message PoseStamped
 import rospkg
 import rospy
 
 
 
 
-def transform_req(req):
+def transform_req(req): # here we call the handler
     rospy.logwarn("AQUI 0")
-    grapes_wrt_tag_t = PoseArray()
+    # grapes_wrt_tag_t = PoseArray()
+    grapes_wrt_tag_t = PoseStampedArray()
     # AprilTagDetectionArray :custom message with header + detections,
     # detections is a custom message with id , size, pose
 
-    apriltag = req.tag
-    grapes_wrt_camera = req.detection # geometry_msgs/PoseArray
-    rospy.logwarn(apriltag.header.frame_id)
-    rospy.logwarn(grapes_wrt_camera)
-    listener = transf.TransformListener()
+    apriltag = req.tag # reading the tag AprilTagDetectionArray msg
+    grapes_wrt_camera = req.detection # reading the geometry_msgs/PoseArray from detection
+    rospy.logwarn(apriltag.header.frame_id) # prints the frame id of apriltags
+    rospy.logwarn(grapes_wrt_camera) # prints the grapes_wrt_camera
+    listener = transf.TransformListener() # creating the transform listener
+    # waiting for the transform between "tag_0" ( "tag_i") and the camera frame apriltag.detections[i].id[0]
+    ################################################################################################ my risky attempt
     listener.waitForTransform("tag_"+str(apriltag.detections[0].id[0]),grapes_wrt_camera.header.frame_id,grapes_wrt_camera.header.stamp,rospy.Duration(3.0))
-    grapes_wrt_tag_t.header.stamp = listener.getLatestCommonTime("tag_"+str(apriltag.detections[0].id[0]),grapes_wrt_camera.header.frame_id)
-    grapes_wrt_tag_t.header.frame_id = "tag_"+str(apriltag.detections[0].id[0])
+    dist=np.zeros((len(grapes_wrt_camera.poses),len(apriltag.detections))) # array containing rows of # grapes and cols of # tags
+    for i in range(len(apriltag.detections)):
+        x_tag = apriltag.detection[i].pose.pose.position.x
+        y_tag = apriltag.detection[i].pose.pose.position.y
+        z_tag = apriltag.detection[i].pose.pose.position.z
+        for j in range(len(grapes_wrt_camera.poses)):
+            x_grape = grapes_wrt_camera.poses[j].position.x
+            y_grape = grapes_wrt_camera.poses[j].position.y
+            z_grape = grapes_wrt_camera.poses[j].position.z
+            dist[j,i]=np.sqrt((x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2) # building distance matrix
 
-    for i in range(len(grapes_wrt_camera.poses)): # grapes_wrt_camera.header.frame_id[i]
-        grapes_wrt_tag_s = PoseStamped()
-        rospy.logwarn(grapes_wrt_camera.poses[i])
-        grapes_wrt_tag_s.header=grapes_wrt_camera.header
-        grapes_wrt_tag_s.pose=grapes_wrt_camera.poses[i]
-        grapes_wrt_tag_t.poses.append(listener.transformPose("tag_"+str(apriltag.detections[0].id[0]),grapes_wrt_tag_s).pose)
+    idx_tag=np.argmin(dist, axis=1) # finds min index by rows "contains the tags id"
+    #idx is a vector composed like this [0 1 0 1 2 2 1 0] where the value corrisponds to the tag and the idx position to the grape.
+    #d_min=dist.min(axis=1)# find minumum distances " contains the minumum dist"
+    for k in range(len(idx)):
+        for t in range(len(grapes_wrt_camera.poses))
+            grapes_wrt_tag_s = PoseStamped()
+            grapes_wrt_tag_s.header.stamp = listener.getLatestCommonTime("tag_"+str(apriltag.detections[k].id[0]),grapes_wrt_camera.header.frame_id) # taking the time
+            grapes_wrt_tag_s.header.frame_id = "tag_"+str(apriltag.detections[k].id[0])# creating the frame ID
+            grapes_wrt_tag_s.pose=grapes_wrt_camera.poses[t] # taking each pose
+            grapes_wrt_tag_t.poses.append(listener.transformPose("tag_"+str(apriltag.detections[k].id[0]),grapes_wrt_tag_s).pose) # transformming and appending the new pose
 
     rospy.logwarn(grapes_wrt_tag_t)
-    return  tree_transformServiceResponse(detection_res=grapes_wrt_tag_t)
+
+    ################################################################################################
+    # listener.waitForTransform("tag_"+str(apriltag.detections[0].id[0]),grapes_wrt_camera.header.frame_id,grapes_wrt_camera.header.stamp,rospy.Duration(3.0))
+    # grapes_wrt_tag_t.header.stamp = listener.getLatestCommonTime("tag_"+str(apriltag.detections[0].id[0]),grapes_wrt_camera.header.frame_id) # taking the time
+    # grapes_wrt_tag_t.header.frame_id = "tag_"+str(apriltag.detections[0].id[0])# creating the frame ID
+    #
+    # for i in range(len(grapes_wrt_camera.poses)): # grapes_wrt_camera.header.frame_id[i]
+    #     grapes_wrt_tag_s = PoseStamped()
+    #     rospy.logwarn(grapes_wrt_camera.poses[i])
+    #     grapes_wrt_tag_s.header=grapes_wrt_camera.header
+    #     grapes_wrt_tag_s.pose=grapes_wrt_camera.poses[i] # taking each pose
+    #     grapes_wrt_tag_t.poses.append(listener.transformPose("tag_"+str(apriltag.detections[0].id[0]),grapes_wrt_tag_s).pose) # transformming and appending the new pose
+    #
+    # rospy.logwarn(grapes_wrt_tag_t)
+    return  tree_transformServiceResponse(detection_res=grapes_wrt_tag_t) # assigning the constructed PoseArray
 
 def tree_transf_server():
-    rospy.init_node('tree_transform_node', anonymous=True)
+    rospy.init_node('tree_transform_node', anonymous=True) # initialize the service node
     rospy.logwarn("AQUI Marco")
-    s = rospy.Service('tree_transf_srv',  tree_transformService, transform_req)
+    s = rospy.Service('tree_transf_srv',  tree_transformService, transform_req) # rospy.Service(name, service_class, handler)
     rospy.spin()
 
 if __name__ == '__main__':
--- tree_transform/scripts/tree_transf_client.py
+++ tree_transform/scripts/tree_transf_client.py
@@ -3,28 +3,28 @@
 import rospy
 import rospkg
 
-from tree_transform.srv import tree_transformService, tree_transformServiceResponse
-from data_fusion_node.msg import PoseArrayId
-from apriltag_ros.msg import AprilTagDetectionArray
+from tree_transform.srv import tree_transformService, tree_transformServiceResponse # importing the srv file top and bottom
+from data_fusion_node.msg import PoseArrayId # importing custom message of Grapes pose arrays
+from apriltag_ros.msg import AprilTagDetectionArray # importing the custom message of apriltags
 
 def get_tree_transf_client():
-    rospy.wait_for_service('tree_transf_srv')
+    rospy.wait_for_service('tree_transf_srv') # wait for service to be available
     try:
-        rospy.logwarn("here")
-        client_handle = rospy.ServiceProxy('tree_transf_srv', tree_transformService)
-	# wait topic
-        tag=rospy.wait_for_message("/tag_detections", AprilTagDetectionArray)
-        detections=rospy.wait_for_message("/poses_pfc", PoseArrayId)
+        rospy.logwarn("here") # service is available
+        client_handle = rospy.ServiceProxy('tree_transf_srv', tree_transformService) # calling the service that we want to call
+     	# wait topic
+        tag=rospy.wait_for_message("/tag_detections", AprilTagDetectionArray) # we wait for AprilTagDetectionArray message
+        detections=rospy.wait_for_message("/poses_pfc", PoseArrayId) # we wait for the PoseArrayID message
 
         #rospy.logwarn(detections.poses)
         rospy.logwarn("here 1")
-        response1 = client_handle(tag,detections.poses)
+        response1 = client_handle(tag,detections.poses) # passing the two received messages to the service and saving the response
         rospy.logwarn("here 2")
-        rospy.logwarn(response1)
+        rospy.logwarn(response1) 
         return response1
     except rospy.ServiceException as e:
         rospy.logerr("Service call failed: %s"%e)
 
 if __name__ == "__main__":
-    rospy.init_node('tree_transform_client')
+    rospy.init_node('tree_transform_client') # initializing client
     get_tree_transf_client()
